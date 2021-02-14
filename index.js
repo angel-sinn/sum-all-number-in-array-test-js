2c9c0979557a7b966d4dd9ae9599acf7e0a0c451
Angel Sinn <angelkysinn@gmail.com>
2021-02-14 02:56:22 +0000

    Sum array function completed

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((el) => {
+    if (Array.isArray(el)) {
+      sum += sumItems(el);
+    } else {
+      sum += el;
+    }
+  });
+  return sum;
 }
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 
 module.exports = sumItems;
